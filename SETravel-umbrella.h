6ce15e639f3fa2e4a34e83d1abf94e2cf6526a6e
SamChenzx <samchen.swift@gmail.com>
2022-01-23 19:44:21 +0800

    static pod init

---
+++ SETravel-umbrella.h
@@ -0,0 +1,30 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "gifTestMockHTTPStubs.h"
+#import "gifTestMockResponse.h"
+#import "gifTestMockServer.h"
+#import "NSURLSessionConfiguration+gifTestMock.h"
+#import "gifTestResourceCDNFetcher.h"
+#import "gifTestResourceDiskCache.h"
+#import "gifTestResourceHelper.h"
+#import "gifTestToolsKeyFileMap.h"
+#import "KSCaseCoverageListView.h"
+#import "KSCoverageInfoView.h"
+#import "KSCoverageManager.h"
+#import "KSFuncCoverageProcessor.h"
+#import "SEObject.h"
+#import "SETravel-Bridging-Header.h"
+
+FOUNDATION_EXPORT double SETravelVersionNumber;
+FOUNDATION_EXPORT const unsigned char SETravelVersionString[];
+
